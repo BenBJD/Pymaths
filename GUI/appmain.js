bfdf04339557fb148830cf77f93d8910907a96f9
Ben Davison <ben@bjdavison.co.uk>
2018-07-09 18:12:47 +0100

    Added tabs to index.html

--- GUI/appmain.js
+++ GUI/appmain.js
@@ -1,8 +1,8 @@
-const {app, BrowserWindow} = require('electron')
-  
-  function createWindow () {
-    win = new BrowserWindow({width: 800, height: 600})
+const { app, BrowserWindow } = require('electron')
+
+function createWindow() {
+    win = new BrowserWindow({ width: 800, height: 400 })
     win.loadFile('index.html')
-  }
-  
-  app.on('ready', createWindow)
+}
+
+app.on('ready', createWindow)
